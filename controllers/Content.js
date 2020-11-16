bfb610b9b22b51c67764c5bf59955939520a8a68
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-11-16 12:35:54 +0530

    Added the controller to
    Add content
    Get all content

---
+++ controllers/Content.js
@@ -0,0 +1,31 @@
+const Content = require("../models/Content");
+
+const getAllContent = async (req, res) => {
+  let content;
+  try {
+    content = await Content.find({}).lean().exec();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Server error occurred. Please try again later" });
+  }
+};
+
+const addContent = async (req, res) => {
+  let { link, course, degree, branches } = req.body;
+  branches = branches.split(",");
+
+  const newContent = new Content({ link, course, degree, branches });
+
+  try {
+    await newContent.save();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Error in adding content. Please try again later" });
+  }
+
+  return res.status(200).json({ message: "Added content sucessfully" });
+};
+
+module.exports = { getAllContent, addContent };
